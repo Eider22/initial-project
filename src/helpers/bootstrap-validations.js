4bbd0b99af777252b3652a43305e01b7da84e685
Eider22 <eidercardona22@gmail.com>
2023-05-27 20:40:32 -0500

    Implementación básica validación form bootstrap - Se comienza a implementar de manera muy básica la validación de   formularios con las validaciones de formulario que provee bootstrap - Bootstrap nos da un código para pegarlo en un archivo js que se   ejecutará al inicio de la puesta en ejecución del proyecto. Este   archivo debemos crearlo para que funcionen los estilos de validaciones   para los  controles(inputs, select..etc) - El archivo que mencioné en el punto anterior, lo cambié y adapté para   que funcionara al evento click del modal que contiene el formulario.   Tambien configué que en el evento click del botón   limpiar del formulario se limpiaran las alertas de errores dadas por   la validación de bootstrap.

---
+++ src/helpers/bootstrap-validations.js
@@ -0,0 +1,31 @@
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+document.addEventListener("DOMContentLoaded", () => {
+  (() => {
+    "use strict";
+    const buttonSubmitForm = document.getElementById("submit-form-button");
+    const buttonCleanForm = document.getElementById("clean-form-button");
+
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    const forms = document.querySelectorAll(".needs-validation");
+
+    buttonSubmitForm.addEventListener(
+      "click",
+      (event) => {
+        Array.from(forms).forEach((form) => {
+          form.classList.add("was-validated");
+        });
+      },
+      false
+    );
+
+    buttonCleanForm.addEventListener(
+      "click",
+      (event) => {
+        Array.from(forms).forEach((form) => {
+          form.classList.remove("was-validated");
+        });
+      },
+      false
+    );
+  })();
+});
