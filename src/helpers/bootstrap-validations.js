4cc79879d4035d6b0b2db748f1de145a0841590b
Eider22 <eidercardona22@gmail.com>
2023-05-29 16:14:15 -0500

    -

--- src/helpers/bootstrap-validations.js
+++ src/helpers/bootstrap-validations.js
@@ -1,32 +1,32 @@
-// Example starter JavaScript for disabling form submissions if there are invalid fields
-document.addEventListener("DOMContentLoaded", () => {
-  (() => {
-    "use strict";
-    const buttonSubmitForm = document.getElementById("submit-form-button");
-    const buttonCleanForm = document.getElementById("clean-form-button");
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to
-    const forms = document.querySelectorAll(".needs-validation");
+// // Example starter JavaScript for disabling form submissions if there are invalid fields
+// document.addEventListener("DOMContentLoaded", () => {
+//   (() => {
+//     "use strict";
+//     const buttonSubmitForm = document.getElementById("submit-form-button");
+//     const buttonCleanForm = document.getElementById("clean-form-button");
+//     // Fetch all the forms we want to apply custom Bootstrap validation styles to
+//     const forms = document.querySelectorAll(".needs-validation");
 
-    buttonSubmitForm.addEventListener(
-      "click",
-      (event) => {
-        Array.from(forms).forEach((form) => {
-          event.stopPropagation();
-          form.classList.add("was-validated");
-        });
-      },
-      false
-    );
+//     buttonSubmitForm.addEventListener(
+//       "click",
+//       (event) => {
+//         Array.from(forms).forEach((form) => {
+//           event.stopPropagation();
+//           form.classList.add("was-validated");
+//         });
+//       },
+//       false
+//     );
 
-    buttonCleanForm.addEventListener(
-      "click",
-      (event) => {
-        Array.from(forms).forEach((form) => {
-          event.stopPropagation();
-          form.classList.remove("was-validated");
-        });
-      },
-      false
-    );
-  })();
-});
+//     buttonCleanForm.addEventListener(
+//       "click",
+//       (event) => {
+//         Array.from(forms).forEach((form) => {
+//           event.stopPropagation();
+//           form.classList.remove("was-validated");
+//         });
+//       },
+//       false
+//     );
+//   })();
+// });
